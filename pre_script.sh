3fc8426922e44d465363fbf9579bad2f0a61c09b
Cheng-Ling Lai <jamesljlster@gmail.com>
2020-10-29 22:07:27 +0800

    Reduce download size

--- pre_script.sh
+++ pre_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y tzdata openssl ca-certificates
+apt-get update && apt-get install --no-install-recommends -y tzdata openssl ca-certificates
 ln -fs /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
